3a7b99df729ddc4a92375fb59cffab4d4e15a01c
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-06-02 14:00:06 +0300

    Docutech edit

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   resources :todos
 
   # root to: 'users#login'
-  resources :users, only: [:index, :create, :destroy, :show]
+  resources :users, only: [:index, :destroy, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  
+  post '/users', to: 'users#create'
   post '/login', to: 'auth#create'
   delete '/logout', to: 'auth#destroy'
   get '/users/login/check', to: 'auth#create'
